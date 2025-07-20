0a75536253eabe63066f49a047bf7bed291c85c2
rachit-developer24 <srachit841@gmail.com>
2025-07-21 00:23:32 +0100

    initial stage

--- FetchImageApi/ContentView.swift
+++ FetchImageApi/ContentView.swift
@@ -7,13 +7,51 @@
 
 import SwiftUI
 
+class serviceimage{
+    func fetchimage()async throws -> UIImage{
+        let urlstring = "https://picsum.photos/200"
+        guard let url = URL(string: urlstring)else{
+            throw URLError(.badURL)
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+      guard let image = UIImage(data: data)else{
+            throw URLError(.badURL)}
+        return image
+        
+        
+    }
+}
+
+class ImageService: ObservableObject {
+    @Published var image : Image?
+    let imageservice = serviceimage()
+    init(){
+        Task{
+            await fetchimage()
+        }
+    }
+    func fetchimage() async {
+        do{
+            let image = try await imageservice.fetchimage()
+            self.image = Image(uiImage: image)
+        }catch {
+            print(error)
+        }
+    }
+}
+
+
 struct ContentView: View {
+    @StateObject var imageService = ImageService()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      if let image = imageService.image {
+                image
+              .resizable()
+              .frame(width: 300, height: 300)
+              .clipShape(.rect(cornerRadius: 10))
+          
+            }
         }
         .padding()
     }
