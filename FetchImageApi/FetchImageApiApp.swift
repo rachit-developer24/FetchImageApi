b3e7b7a10bad98ccd004eadaff2f7420c115df46
rachit-developer24 <srachit841@gmail.com>
2025-07-20 23:47:15 +0100

    Initial Commit

---
+++ FetchImageApi/FetchImageApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchImageApiApp.swift
+//  FetchImageApi
+//
+//  Created by Rachit Sharma on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchImageApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchImageApiTests/FetchImageApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchImageApiTests.swift
+//  FetchImageApiTests
+//
+//  Created by Rachit Sharma on 20/07/2025.
+//
+
+import Testing
+@testable import FetchImageApi
+
+struct FetchImageApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
